b307fff1038a6fa8859e1d3a02655896ec4e8198
Chacha Marwa <chachamarwa003@gmail.com>
2026-03-31 15:37:42 +0300

    docs: add problem context and rules for March 25 problem
    - Add problem statement and sample outputs to README
    - Define solution approach as comments in march_25.py

--- march_25.py
+++ march_25.py
@@ -1,1 +1,8 @@
+# Rules
+
+# if a warmer day is j and normal day is i then output[i] = j - i
+# warmer means strictly greater temperature, equal temp = 0 count
+# if no warmer day exist = 0
+# output array is the same as input array
+
 
