54bcbca276de6f8cbb11564c2e8db166594ccf52
Chacha Marwa <chachamarwa003@gmail.com>
2026-05-27 06:36:15 +0300

    Implement recursive binary_search and use it
    Replace the previous test scaffolding with a recursive binary_search function that handles empty lists, checks the middle element, and recurses on the appropriate half. Removed an unused loop and the previous len() print; added a call to print(binary_search(test_list, 2)) and a small comment noting the list length.

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-test_list = [1, 2, 3, 5, 6, 7, 9]
+test_list = [1, 2, 3, 5, 6, 7, 9] 
+# no. of items = 7
 
-print(len(test_list))
+def binary_search(test_list, target):
+    if not test_list:
+        return None
+    
+    mid = len(test_list) // 2
 
-for item in test_list:
-    if item == 0:
-        item += 1
+    if test_list[mid] == target:
+        return test_list[mid]
+    elif target < test_list[mid]:
+        return binary_search(test_list[:mid], target)
+    else:
+        return binary_search(test_list[mid + 1:], target)
 
 
+
+print(binary_search(test_list, 2))
 
 
