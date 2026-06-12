2ba2079ee1cb550e642d4c837c6bf751c49f1520
Chacha Marwa <chachamarwa003@gmail.com>
2026-06-12 10:02:01 +0300

    Update moved the logic to class

--- learning/python_oop/01_classes_basic.py
+++ learning/python_oop/01_classes_basic.py
@@ -13,3 +13,10 @@ client = RESTClient(api_key=api_key, api_secret=api_secret, timeout=5)
 raw = client.get_products()
 data = raw.to_dict()
 
+products = data["products"][0]
+
+class Coinbase:
+    def __init__(self, client):
+        self.raw = client.get_products()
+        self.data = self.raw.to_dict()
+        self.product = self.data["products"][0]
